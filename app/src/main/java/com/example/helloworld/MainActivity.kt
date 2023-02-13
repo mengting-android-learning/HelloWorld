7e26e461fe26ccaeaa6655d9c782490f9d2270bf
Rachel-oc <mengting.xie@thoughtworks.com>
2023-02-13 10:13:12 +0800

    [feat]:replace text with images

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,24 +19,30 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
-//        Toast.makeText(this, "button clicked",
-//            Toast.LENGTH_SHORT).show()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 
     private fun countUp(){
-        val resultText: TextView = findViewById(R.id.result_text)
-        if(resultText.text.toString() == "Hello World!"){
-            resultText.text = "1"
-        }else if (resultText.text.toString().toInt() < 6){
-            val resultInt = resultText.text.toString().toInt()+1
-            resultText.text = resultInt.toString()
-        }
+//        val resultText: TextView = findViewById(R.id.result_text)
+//        if(resultText.text.toString() == "Hello World!"){
+//            resultText.text = "1"
+//        }else if (resultText.text.toString().toInt() < 6){
+//            val resultInt = resultText.text.toString().toInt()+1
+//            resultText.text = resultInt.toString()
+//        }
     }
 
     private fun reset(){
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "0"
+//        val resultText: TextView = findViewById(R.id.result_text)
+//        resultText.text = "0"
     }
 }
